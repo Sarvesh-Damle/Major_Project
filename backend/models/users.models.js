248d7903eb662b5a33c79ba1b0fc608b0f44dcb9
Sarvesh <sarvesh.damle1@gmail.com>
2024-02-13 09:51:06 +0530

    Some frontend-backend modifications

---
+++ backend/models/users.models.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const usersSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, {timestamps: true});
+
+export default mongoose.model("User", usersSchema);
