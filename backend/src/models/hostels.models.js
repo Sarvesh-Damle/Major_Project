efd09572f00e55434fe0ccd1ef090919afcf4f0d
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-06 12:13:31 +0530

    Model changes

--- backend/src/models/hostels.models.js
+++ backend/src/models/hostels.models.js
@@ -1,60 +1,81 @@
 import mongoose from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const hostelsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const hostelsSchema = new mongoose.Schema(
+  {
+    hostel_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    ownerName: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    type_of_hostel: {
+      type: String,
+      required: true,
+    },
+    room_type: {
+      type: String,
+      required: true,
+    },
+    rent_amount: {
+      type: Number,
+      required: true,
+    },
+    distance_from_nearest_railway_station: {
+      type: Number,
+      required: true,
+    },
+    distance_from_bus_stop: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    facilities: {
+      type: [String],
+      required: true,
+    },
+    rules: {
+      type: [String],
+      required: true,
+    },
+    amenities: {
+      type: [String],
+      required: true,
+    },
+    security_deposit: {
+      type: Number,
+      required: true,
+    },
+    photos: {
+      type: [String],
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
   },
-  type: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  distance: {
-    type: String,
-    required: true,
-  },
-  photos: {
-    type: [String],
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: Number,
-    min: 0,
-    max: 5,
-  },
-  room_type: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  tenant_type: {
-    type: String,
-    required: true,
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 hostelsSchema.plugin(mongooseAggregatePaginate);
 
