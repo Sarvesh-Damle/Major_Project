4f1c3c1784fc5230428fb42a197c673949c76010
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-06 12:11:15 +0530

    Installed React Query and implemented required settings

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,30 +4,35 @@ import App from './App.jsx';
 import './index.css';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import { Home, SignIn, SignUp, About, Property, Contact, Error, Team, ListProperty, Students, Professionals, PropertyOwner, ResetPassword } from './components/index.js';
+import Provider from './components/provider.jsx';
+import { ToastContainer } from "react-toastify";
 
 const router = createBrowserRouter(createRoutesFromElements(
   <>
-  <Route path='/' element={<App />}>
-    <Route path='' element={<Home />} />
-    <Route path='signin' element={<SignIn />} />
-    <Route path='signup' element={<SignUp />} />
-    <Route path='about' element={<About />} />
-    <Route path='property' element={<Property />} />
-    <Route path='contact' element={<Contact />} />
-    <Route path='team' element={<Team />} />
-    <Route path='list' element={<ListProperty />} />
-    <Route path='students' element={<Students />} />
-    <Route path='professionals' element={<Professionals />} />
-    <Route path='owner' element={<PropertyOwner />} />
-    <Route path='error' element={<Error />} />
-    <Route path='reset-password' element={<ResetPassword/>} />
-  </Route>
-  <Route path='*' element={<Error/>}/>
+    <Route path='/' element={<App />}>
+      <Route path='' element={<Home />} />
+      <Route path='signin' element={<SignIn />} />
+      <Route path='signup' element={<SignUp />} />
+      <Route path='about' element={<About />} />
+      <Route path='property' element={<Property />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='team' element={<Team />} />
+      <Route path='list' element={<ListProperty />} />
+      <Route path='students' element={<Students />} />
+      <Route path='professionals' element={<Professionals />} />
+      <Route path='owner' element={<PropertyOwner />} />
+      <Route path='error' element={<Error />} />
+      <Route path='reset-password' element={<ResetPassword />} />
+    </Route>
+    <Route path='*' element={<Error />} />
   </>
 ))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ToastContainer autoClose={3000} />
+    <Provider>
+      {<RouterProvider router={router} />}
+    </Provider>
   </React.StrictMode>,
 )
