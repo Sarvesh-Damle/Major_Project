da7f0ec04c88f5e8e830b2469c126980b1c6f9f9
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-10 18:58:56 +0530

    created a constant data file with property details like amenities, rules

---
+++ frontend/src/data/Property.js
@@ -0,0 +1,13 @@
+export const amenities = [
+  "Air conditioning and necessary furniture",
+  "Regular Housekeeping",
+  "Laundry Facilities",
+  "Wi-Fi",
+];
+
+export const rules = [
+  "Pets Not Allowed",
+  "No Drinking",
+  "No Smoking",
+  "No Opposite Gender",
+];
