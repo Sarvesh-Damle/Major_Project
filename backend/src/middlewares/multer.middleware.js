07ea68ae89827ee8c3e02f511c954e2b8db283c4
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-13 21:00:20 +0530

    some minor modifications

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -2,11 +2,11 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    return cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    return cb(null, file.fieldname + "-" + uniqueSuffix);
   },
 });
 
