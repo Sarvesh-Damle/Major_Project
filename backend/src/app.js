936cb50d08df972c271bcb73038762e292d16f8d
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-13 20:59:50 +0530

    added routes for PGs

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.routes.js";
 import usersRoute from "./routes/users.routes.js";
 import hostelsRoute from "./routes/hostels.routes.js";
 import flatsRoute from "./routes/flats.routes.js";
+import pgsRoute from "./routes/pgs.routes.js";
 
 const app = express();
 
@@ -13,8 +14,8 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
@@ -23,6 +24,7 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", usersRoute);
 app.use("/api/v1/flats", flatsRoute);
 app.use("/api/v1/hostels", hostelsRoute);
+app.use("/api/v1/pgs", pgsRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
