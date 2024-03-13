d1c56cf24962330ceaa20410a4a19c9eba9fc847
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-13 20:56:45 +0530

    This is data file for adding all the constant data values

--- frontend/src/data/Property.js
+++ frontend/src/data/Property.js
@@ -1,8 +1,15 @@
-export const amenities = [
+export const pg_amenities = [
   "Air conditioning and necessary furniture",
   "Regular Housekeeping",
   "Laundry Facilities",
   "Wi-Fi",
+];
+
+export const flat_amenities = [
+  "Gym Available",
+  "Swimming pool",
+  "Security",
+  "Balcony",
 ];
 
 export const rules = [
@@ -11,3 +18,15 @@ export const rules = [
   "No Smoking",
   "No Opposite Gender",
 ];
+
+export const flat_types = [
+  "1 BHK",
+  "2 BHK",
+  "3 BHK",
+];
+
+export const furnished_status = ["Fully Furnished", "Semi-Furnished", "Unfurnished"]
+
+export const hostel_types = ["Boys-Hostel", "Girls-Hostel"];
+
+export const room_types = ["Single", "Double-Sharing", "Triple-Sharing", "Four-Sharing"];
