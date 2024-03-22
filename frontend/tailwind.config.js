79f7ee8c0cc8f38da5e46f53b9921257150accf2
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-22 12:50:41 +0530

    added custom stylings

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        black: 'var(--black)',
+        blue: 'var(--blue)',
+        lightBlue: 'var(--lightBlue)',
+        shadow: 'var(--shadow)',
+        'gradient-white-blue': 'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(136, 160, 255, 0.46) 217.91%)',
+      },
+      backgroundImage: {
+        'blue-gradient': 'var(--blue-gradient)',
+        'orange-gradient': 'var(--orange-gradient)',
+      }
+    },
   },
   plugins: [],
 }
