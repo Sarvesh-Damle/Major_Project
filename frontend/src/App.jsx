d56869cae9a1e563e8fca395fde1eb69b66bc748
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-22 12:57:35 +0530

    added an api call to view if user is logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import { Footer, Navbar } from './components';
-
+import {  Navbar, Footer } from './components';
+import axios from "axios";
+import { useContext } from 'react';
+import { loginContext } from "./provider/authContext";
 function App() {
+  const {setIsLoggedIn:isAuthenticated}=useContext(loginContext)
+
+  useEffect(() => {
+    async function callData() {
+      try {
+        const res = await axios.get("/api/v1/users/me", { withCredentials: true });
+        if(res.data.data._id){
+          isAuthenticated({login: true, signup: true})
+        }
+        else{
+           isAuthenticated({login: false, signup: false})
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    callData();
+  }, []);
   return (
     <>
-    <div className='overflow-hidden'>
+    <div className="relative overflow-hidden">
       <Navbar />
       <Outlet />
       <Footer />
