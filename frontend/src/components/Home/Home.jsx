2e303182672d05a26aaa034a72e7870c42b4bf97
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-22 13:00:18 +0530

    Modified Home Page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -7,19 +7,25 @@ import { Outlet } from 'react-router-dom';
 import SearchBar from './SearchBar.jsx';
 import Testimonials from './Testimonials/Testimonials.jsx';
 import testimonialsData from './Testimonials/testimonialsData.js';
+import Hero from './Hero.jsx';
+import Properties from './Properties.jsx';
+import Value from './Value/Value.jsx';
 
 const Home = () => {
   return (
     <div>
-      <SearchBar/>
-      <Featured />
-      <Rooms />
-      <h1 className="flex justify-center items-center text-3xl p-1 my-2">Flats/Rooms</h1>
+      <Hero/>
+      {/* <SearchBar/> */}
+      <Properties/>
+      <Value/>
+      {/* <Featured /> */}
+      {/* <Rooms /> */}
+      {/* <h1 className="flex justify-center items-center text-3xl p-1 my-2">Flats/Rooms</h1>
       <Card image="https://res.cloudinary.com/sarvesh-damle/image/upload/v1696489671/Buddies_MajorProject/property_images/norbert-levajsics-oTJ92KUXHls-unsplash_1_ssb2ni.jpg" cardarray={CardArray1} />
       <h1 className="flex justify-center items-center text-3xl p-1 my-2">Apartments/PG/Hostels</h1>
       <Card image="https://res.cloudinary.com/sarvesh-damle/image/upload/v1699037701/Buddies_MajorProject/property_images/hostel_image_sgsi52.jpg" cardarray={CardArray2} />
       <h1 className="flex justify-center items-center text-3xl p-1 my-2">Flatmates</h1>
-      <Flatmates />
+      <Flatmates /> */}
       <Testimonials testimonials={testimonialsData}/>
       <Outlet />
     </div>
