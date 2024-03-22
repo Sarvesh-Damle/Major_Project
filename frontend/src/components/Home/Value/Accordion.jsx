cffcf8285956055d6afafeadd685200602d4fde3
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-22 13:01:55 +0530

    Added Accordion Component

---
+++ frontend/src/components/Home/Value/Accordion.jsx
@@ -0,0 +1,23 @@
+import {HiShieldCheck} from "react-icons/hi";
+import {MdCancel, MdAnalytics} from "react-icons/md";
+
+export const accordion_data = [
+    {
+      icon: <HiShieldCheck/>,
+      heading: "Verified Properties",
+      detail:
+        "All the properties listed on our platform have been verified, and there is regular verification of the properties.",
+    },
+    {
+      icon: <MdCancel />,
+      heading: "No Brokerage",
+      detail:
+        `Our Motto is "Let People help other people", the properties listed on our platform have been through college students, working professionals and the property owners, thus eliminating the middle-men and saving brokerage costs for both parties.`,
+    },
+    {
+      icon: <MdAnalytics />,
+      heading: "Get properties closer to your work",
+      detail:
+        "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim minim esse tempor laborum consequat esse adipisicing eu reprehenderit enim.",
+    },
+  ]
