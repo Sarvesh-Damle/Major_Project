e60afac0839b9053d51f42c5a32dd2f02c51d4aa
Sarvesh <sarvesh.damle1@gmail.com>
2024-03-22 13:04:50 +0530

    Added static property related data

--- frontend/src/data/Property.js
+++ frontend/src/data/Property.js
@@ -19,14 +19,64 @@ export const rules = [
   "No Opposite Gender",
 ];
 
-export const flat_types = [
-  "1 BHK",
-  "2 BHK",
-  "3 BHK",
-];
+export const flat_types = ["1 BHK", "2 BHK", "3 BHK"];
 
-export const furnished_status = ["Fully Furnished", "Semi-Furnished", "Unfurnished"]
+export const furnished_status = [
+  "Fully Furnished",
+  "Semi-Furnished",
+  "Unfurnished",
+];
 
 export const hostel_types = ["Boys-Hostel", "Girls-Hostel"];
 
-export const room_types = ["Single", "Double-Sharing", "Triple-Sharing", "Four-Sharing"];
+export const room_types = [
+  "Single",
+  "Double-Sharing",
+  "Triple-Sharing",
+  "Four-Sharing",
+];
+
+export const propertiesData = [
+  {
+    name: "Aliva Priva Jardin",
+    price: "47,043",
+    detail:
+      "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
+    image: "https://res.cloudinary.com/sarvesh-damle/image/upload/v1699037701/Buddies_MajorProject/property_images/hostel_image_sgsi52.jpg",
+  },
+  {
+    name: "Asatti Garden City",
+    price: "66,353",
+    detail:
+      "Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat",
+    image: "https://res.cloudinary.com/sarvesh-damle/image/upload/v1696489671/Buddies_MajorProject/property_images/norbert-levajsics-oTJ92KUXHls-unsplash_1_ssb2ni.jpg",
+  },
+  {
+    name: "Citralan Puri Serang",
+    price: "35,853",
+    detail:
+      "Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten",
+    image: "https://res.cloudinary.com/sarvesh-damle/image/upload/v1699037701/Buddies_MajorProject/property_images/hostel_image_sgsi52.jpg",
+  },
+  {
+    name: "Aliva Priva Jardin",
+    price: "47,043",
+    detail:
+      "Jakarta Garden City Street, Cakung. Pulo Gadung, Jakarta Timur, DKI Jakarta",
+    image: "https://res.cloudinary.com/sarvesh-damle/image/upload/v1696489671/Buddies_MajorProject/property_images/norbert-levajsics-oTJ92KUXHls-unsplash_1_ssb2ni.jpg",
+  },
+  {
+    name: "Asatti Garden City",
+    price: "66,353",
+    detail:
+      "Pahlawan Street XVII No.215, Cinangka, Sawangan, Depok, Jawa Barat",
+    image: "https://res.cloudinary.com/sarvesh-damle/image/upload/v1699037701/Buddies_MajorProject/property_images/hostel_image_sgsi52.jpg",
+  },
+  {
+    name: "Citralan Puri Serang",
+    price: "35,853",
+    detail:
+      "Ruko Puri Indah Residence Block A7, Lingkar Street, Ciracas, Serang, Banten",
+    image: "https://res.cloudinary.com/sarvesh-damle/image/upload/v1696489671/Buddies_MajorProject/property_images/norbert-levajsics-oTJ92KUXHls-unsplash_1_ssb2ni.jpg",
+  },
+];
