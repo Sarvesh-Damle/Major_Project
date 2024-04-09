640da4a3df571a27d521cfca5adfdad82fd2efdd
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-09 11:21:34 +0530

    Location marker logic with address details

---
+++ frontend/src/components/ListProperties/GeoCoderMarker/GeoCoderMarker.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { Marker, Popup, useMap } from "react-leaflet"
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import * as ELG from "esri-leaflet-geocoder";
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const GeoCoderMarker = ({ address }) => {
+    const map = useMap();
+    const [position, setPosition] = useState([19.2, 72.8])
+
+    useEffect(() => {
+      ELG.geocode().text(address).run((err, results, response) => {
+        if (err) {
+            console.error("Error geocoding address:", err);
+            return;
+        }
+        if (results?.results?.length > 0) {
+            const {lat, lng} = results?.results[0].latlng;
+            map.flyTo([lat, lng], 13, {duration: 3})
+            setPosition([lat, lng]);
+        } else {
+            console.warn("No results found for address:", address);
+        }
+      });
+    }, [address, map]);
+    
+
+    return (
+        <Marker position={position} icon={DefaultIcon}>
+            <Popup />
+        </Marker>
+    )
+}
+
+export default GeoCoderMarker
