70aab70c479eba7bed2ddfe795a65f216ede3cbc
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-09 11:26:22 +0530

    solved image-mismatch issue

--- frontend/src/components/Pages/Team.jsx
+++ frontend/src/components/Pages/Team.jsx
@@ -45,30 +45,13 @@ const TeamCard = ({ imageSrc, name, profession }) => {
         <>
             <div className="w-full px-4 md:w-1/2 xl:w-1/4">
                 <div className="mx-auto mb-10 w-full max-w-[370px]">
-                    <div className="relative overflow-hidden rounded-lg">
-                        <img src={imageSrc} alt="" className="w-full" />
+                    <div className="relative overflow-hidden rounded-lg h-[420px]">
+                        <img src={imageSrc} alt="" className="w-full h-full object-cover" />
                         <div className="absolute left-0 w-full text-center bottom-5">
                             <div className="relative px-3 py-5 mx-5 overflow-hidden bg-white rounded-lg">
                                 <h3 className="text-base font-semibold text-dark">{name}</h3>
                                 <p className="text-sm text-body-color">{profession}</p>
                                 <div>
-                                    <span className="absolute bottom-0 left-0">
-                                        <svg
-                                            width={61}
-                                            height={30}
-                                            viewBox="0 0 61 30"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <circle
-                                                cx={16}
-                                                cy={45}
-                                                r={45}
-                                                fill="#13C296"
-                                                fillOpacity="0.11"
-                                            />
-                                        </svg>
-                                    </span>
                                     <span className="absolute top-0 right-0">
                                         <svg
                                             width={20}
