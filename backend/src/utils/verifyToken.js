8ef235669632b450c81115543af781c1df2492f2
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-09 11:12:31 +0530

    updated logic for verifying jwt tokens

--- backend/src/utils/verifyToken.js
+++ backend/src/utils/verifyToken.js
@@ -2,16 +2,17 @@ import jwt from "jsonwebtoken";
 import { createError } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
+  const token = req.cookies.accessToken;
   if (!token) {
     return next(createError(401, "You are not authenticated!"));
   }
 
-  jwt.verify(token, process.env.JWT, (err, user) => {
+  let data=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
+  if(!data){
     if (err) return next(createError(403, "Token is not valid"));
-    req.user = user;
-    next();
-  });
+  }
+  req.user=data;
+  next();
 };
 
 export const verifyUser = (req, res, next) => {
