d4fb9bd57bff81559d03c58d13e700ba8dbfce65
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 21:55:44 +0530

    Restructured main file by shifting routes to App file and keeping it clean and readable

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,83 +1,14 @@
-import React, { useEffect, useState, Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
-import { Home, SignIn, SignUp, Contact, Error, Team, ListProperty, Students, Professionals, PropertyOwner, ResetPassword, Property, HostelProperties, PGProperties, FlatProperties, HostelProperty, FlatProperty, PGProperty, Profile } from './components/index.js';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import Provider from './components/Provider.jsx';
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { loginContext } from './provider/authContext.js';
-import axios from 'axios';
-import { useContext } from 'react';
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <>
-    <Route path='/' element={<App />}>
-      <Route path='' element={<Suspense fallback={<div>Loading...</div>}><Home /></Suspense>} />
-      <Route path='signin' element={<SignIn />} />
-      <Route path='signup' element={<SignUp />} />
-      <Route path='hostels'>
-        <Route path='' element={<HostelProperties />} />
-        <Route path=':propertyId' element={<HostelProperty/>} />
-      </Route>
-      <Route path='pgs'>
-        <Route path='' element={<PGProperties />} />
-        <Route path=':propertyId' element={<PGProperty/>} />
-      </Route>
-      <Route path='flats'>
-        <Route path='' element={<FlatProperties />} />
-        <Route path=':propertyId' element={<FlatProperty/>} />
-      </Route>
-      <Route path='profile'>
-        <Route path='' element={<Profile/>} />
-      </Route>
-      <Route path='contact' element={<Contact />} />
-      <Route path='team' element={<Team />} />
-      <Route path='list' element={<ListProperty />} />
-      <Route path='students' element={<Students />} />
-      <Route path='professionals' element={<Professionals />} />
-      <Route path='owner' element={<PropertyOwner />} />
-      <Route path='error' element={<Error />} />
-      <Route path='reset-password' element={<ResetPassword />} />
-    </Route>
-    <Route path='*' element={<Error />} />
-  </>
-))
-
-export const AppWrapper = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState({ login: false, signup: false });
-  const { setIsLoggedIn: isAuthenticated } = useContext(loginContext)
-
-  useEffect(() => {
-    async function callData() {
-      try {
-        const res = await axios.get("/api/v1/users/me", { withCredentials: true });
-        if (res.data.data._id) {
-          isAuthenticated({ login: true, signup: false })
-        }
-        else {
-          isAuthenticated({ login: false, signup: true })
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    callData();
-  }, []);
-
-  return (
-    <React.StrictMode>
-      <ToastContainer autoClose={1000} />
-      <Provider>
-        <loginContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <RouterProvider router={router} />
-          </Suspense>
-        </loginContext.Provider>
-      </Provider>
-    </React.StrictMode>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById('root')).render(<AppWrapper />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Provider>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
