ce9f684b109b0e796af46cab28ec6f2407b96510
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 21:46:57 +0530

    Checks for type of file and file size

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -5,9 +5,32 @@ const storage = multer.diskStorage({
     return cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
+    console.log(req);
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     return cb(null, file.fieldname + "-" + uniqueSuffix);
   },
 });
 
-export const upload = multer({ storage, });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('File is not an image!'), false);
+  }
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
+});
+
+// Middleware to handle file upload errors
+export const handleFileUploadError = (err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    return res.status(400).json({ error: "File upload error: " + err.message });
+  } else if (err) {
+    return res.status(400).json({ error: err.message });
+  }
+  next();
+};
