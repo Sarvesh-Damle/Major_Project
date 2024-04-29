63fd46b6637da55eb745a748750cab819c49a1ea
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 21:45:10 +0530

    Added logic for uploading multiple photos through multer on cloudinary

--- backend/src/controllers/hostels.controllers.js
+++ backend/src/controllers/hostels.controllers.js
@@ -2,7 +2,8 @@ import Hostel from "../models/hostels.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import { uploadFilesToCloudinary } from "../utils/cloudinary.js";
+import axios from "axios";
 
 export const createHostel = asyncHandler(async (req, res) => {
   const hostelData = req.body;
@@ -10,13 +11,10 @@ export const createHostel = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Fields are required for property to be listed");
   }
   let propertyPhotosUrls = [];
-  if (
-    req.files?.property_photos &&
-    Array.isArray(req.files.property_photos)
-  ) {
+  if (req.files?.property_photos && Array.isArray(req.files?.property_photos)) {
     for (let i = 0; i < Math.min(req.files.property_photos.length, 5); i++) {
       const photosLocalPath = req.files.property_photos[i].path;
-      const propertyPhoto = await uploadOnCloudinary(photosLocalPath);
+      const propertyPhoto = await uploadFilesToCloudinary(photosLocalPath);
       if (!propertyPhoto) {
         throw new ApiError(400, "Failed to upload property photo");
       }
@@ -27,21 +25,66 @@ export const createHostel = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Please provide property photos");
   }
 
-  const hostel = await Hostel.create({...hostelData, property_photos: propertyPhotosUrls});
+  const hostel = await Hostel.create({
+    ...hostelData,
+    property_photos: propertyPhotosUrls,
+  });
   if (!hostel) {
     throw new ApiError(500, "Something went wrong while listing the property");
   }
-
+  // const sendEmail = await axios.post("http://localhost:4000/backend-email-service/email", {
+  //   to: hostelData.email,
+  //   subject: "Property Registration Process has began!",
+  //   body: `   Thank you, for providing property details!!\n\nOur Team will verify the property and will surely get back to you`,
+  //   user: "Buddies.com",
+  // });
+  // if (!sendEmail) {
+  //   throw new ApiError(500, "Something went wrong while sending email");
+  // }
   return res
     .status(201)
     .json(new ApiResponse(200, hostel, "Hostel Property listed successfully"));
 });
 
 export const updateHostel = asyncHandler(async (req, res) => {
-  const {id} = req.query;
+  const {
+    hostelName,
+    typeOfHostel,
+    locality,
+    rentAmount,
+    securityDeposit,
+    verified,
+    city,
+    ownerName,
+    ownerEmail,
+    ownerPhoneNumber,
+    pincode,
+    address,
+    distanceFromRailwayStation,
+    distanceFromBusStop,
+    description,
+  } = req.body;
   const updatedHostel = await Hostel.findByIdAndUpdate(
-    id,
-    { $set: req.body },
+    req.query.id,
+    {
+      $set: {
+        hostel_name: hostelName,
+        type_of_hostel: typeOfHostel,
+        locality,
+        rent_amount: rentAmount,
+        security_deposit: securityDeposit,
+        featured: verified,
+        city,
+        owner_name: ownerName,
+        owner_email: ownerEmail,
+        owner_phoneNumber: ownerPhoneNumber,
+        pincode,
+        address,
+        distance_from_nearest_railway_station: distanceFromRailwayStation,
+        distance_from_bus_stop: distanceFromBusStop,
+        description,
+      },
+    },
     { new: true }
   );
   if (!updatedHostel) {
@@ -53,8 +96,12 @@ export const updateHostel = asyncHandler(async (req, res) => {
 });
 
 export const deleteHostel = asyncHandler(async (req, res) => {
-  const {id} = req.query;
-  await Hostel.findByIdAndDelete(id);
+  const hostel = await Hostel.findById(req.query.id);
+  if (!hostel) {
+    return res.status(200).json(new ApiResponse(200, "Hostel not found"));
+  } else {
+    await Hostel.findByIdAndDelete(req.query.id);
+  }
   return res.status(200).json(new ApiResponse(200, "Hostel has been deleted"));
 });
 
@@ -72,17 +119,22 @@ export const getHostel = asyncHandler(async (req, res) => {
 });
 
 export const getAllHostel = asyncHandler(async (req, res) => {
-  const {city, locality} = req.query;
+  const { city, locality, typeOfHostel, sharingType } = req.query;
   if (!city) {
     throw new ApiError(400, "City paramater not found");
   }
-  let query = {};
-  if (city) {
-    query.city = {$regex: new RegExp(city, "i")};
-  }
+  let query = { city: { $regex: new RegExp(city, "i") } };
+
   if (locality) {
-    query.locality = {$regex: new RegExp(locality, "i")};
+    query.locality = { $regex: new RegExp(locality, "i") };
   }
+  if (typeOfHostel) {
+    query.typeOfHostel = { $in: Array.isArray(typeOfHostel) ? typeOfHostel : [typeOfHostel] };
+  }
+  if (sharingType) {
+    query.sharingType = { $in: Array.isArray(sharingType) ? sharingType : [sharingType] };
+  }
+  query.featured = true;
   const hostels = await Hostel.find(query);
 
   if (!hostels) {
@@ -94,6 +146,54 @@ export const getAllHostel = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json(new ApiResponse(200, hostels, "Hostels found successfully"));
+});
+
+export const getAllHostelsInfo = asyncHandler(async (req, res) => {
+  const hostels = await Hostel.find();
+  if (!hostels) {
+    throw new ApiError(
+      500,
+      "Something went wrong while fetching Hostel properties"
+    );
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(200, hostels, "Hostels found successfully"));
+});
+
+export const countVerifiedHostels = asyncHandler(async (req, res) => {
+  const count = await Hostel.countDocuments({ featured: true });
+  res
+    .status(200)
+    .json(new ApiResponse(200, count, "Counted Verified Hostels successfully"));
+});
+
+export const countNotVerifiedHostels = asyncHandler(async (req, res) => {
+  const count = await Hostel.countDocuments({ featured: false });
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, count, "Counted Unverified Hostels successfully")
+    );
+});
+
+export const countHostelsByType = asyncHandler(async (req, res) => {
+  const boysHostelsCount = await Hostel.countDocuments({
+    type_of_hostel: "Boys-Hostel",
+  });
+  const girlsHostelsCount = await Hostel.countDocuments({
+    type_of_hostel: "Girls-Hostel",
+  });
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        [boysHostelsCount, girlsHostelsCount],
+        "Counted Hostels by type successfully"
+      )
+    );
 });
 
 export const countByAddress = async (req, res, next) => {
@@ -134,3 +234,17 @@ export const countByType = async (req, res, next) => {
     next(error);
   }
 };
+
+export const verifyHostel = asyncHandler(async (req, res) => {
+  const { id, featured } = req.body;
+  await Hostel.findByIdAndUpdate(id, { $set: { featured } }, { new: true });
+  await axios.post("http://localhost:4000/backend-email-service/email", {
+    to: hostelData.email,
+    subject: "Property Listed Successfully!",
+    body: `   Thank you, your property has been listed!!`,
+    user: "Buddies.com",
+  });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Hostel verified successfully"));
+});
--- backend/src/controllers/pgs.controllers.js
+++ backend/src/controllers/pgs.controllers.js
@@ -2,12 +2,13 @@ import PG from "../models/pgs.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { uploadFilesToCloudinary } from "../utils/cloudinary.js";
+import axios from "axios";
 
 export const createPG = asyncHandler(async (req, res) => {
   const pgData = req.body;
   if (!pgData) {
-    throw new ApiError(400, "Fields are required for property to be listed")
+    throw new ApiError(400, "Fields are required for property to be listed");
   }
   let propertyPhotosUrls = [];
   if (
@@ -17,7 +18,7 @@ export const createPG = asyncHandler(async (req, res) => {
   ) {
     for (let i = 0; i < Math.min(req.files.property_photos.length, 5); i++) {
       const photosLocalPath = req.files.property_photos[i].path;
-      const propertyPhoto = await uploadOnCloudinary(photosLocalPath);
+      const propertyPhoto = await uploadFilesToCloudinary(photosLocalPath);
       if (!propertyPhoto) {
         throw new ApiError(400, "Failed to upload property photo");
       }
@@ -28,20 +29,29 @@ export const createPG = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Please provide property photos");
   }
 
-  const pg = await PG.create({...pgData, property_photos: propertyPhotosUrls});
+  const pg = await PG.create({
+    ...pgData,
+    property_photos: propertyPhotosUrls,
+  });
   if (!pg) {
     throw new ApiError(500, "Something went wrong while listing the property");
   }
-
+  await axios.post("http://localhost:4000/backend-email-service/email", {
+    to: hostelData.email,
+    subject: "Property Registration Process has began!",
+    body: `   Thank you, for providing property details!!\n\nOur Team will verify the property and will surely get back to you`,
+    user: "Buddies.com",
+  });
   return res
     .status(201)
     .json(new ApiResponse(200, pg, "PG Property listed successfully"));
 });
 
 export const updatePG = asyncHandler(async (req, res) => {
+  const {pgName, preferredTennats, locality, rentAmount, securityDeposit, verified, city, ownerName, ownerEmail, ownerPhoneNumber, pincode, address, distanceFromRailwayStation, distanceFromBusStop, description, foodIncluded} = req.body;
   const updatedPG = await PG.findByIdAndUpdate(
     req.query.id,
-    { $set: req.body },
+    { $set: {pg_name: pgName, preferred_tennats: preferredTennats, locality, rent_amount: rentAmount, security_deposit: securityDeposit, featured: verified, city, owner_name: ownerName, owner_email: ownerEmail, owner_phoneNumber: ownerPhoneNumber, pincode, address, distance_from_nearest_railway_station: distanceFromRailwayStation, distance_from_bus_stop: distanceFromBusStop, description, food_included: foodIncluded} },
     { new: true }
   );
   if (!updatedPG) {
@@ -53,7 +63,12 @@ export const updatePG = asyncHandler(async (req, res) => {
 });
 
 export const deletePG = asyncHandler(async (req, res) => {
-  await PG.findByIdAndDelete(req.query.id);
+  const pg = await PG.findById(req.query.id);
+  if (!pg) {
+    return res.status(200).json(new ApiResponse(200, "PG not found"));
+  } else {
+    await PG.findByIdAndDelete(req.query.id);
+  }
   return res.status(200).json(new ApiResponse(200, "PG has been deleted"));
 });
 
@@ -71,17 +86,18 @@ export const getPG = asyncHandler(async (req, res) => {
 });
 
 export const getAllPG = asyncHandler(async (req, res) => {
-  const {city, locality} = req.query;
+  const { city, locality } = req.query;
   if (!city) {
     throw new ApiError(400, "City paramater not found");
   }
   let query = {};
   if (city) {
-    query.city = {$regex: new RegExp(city, "i")};
+    query.city = { $regex: new RegExp(city, "i") };
   }
   if (locality) {
-    query.locality = {$regex: new RegExp(locality, "i")};
+    query.locality = { $regex: new RegExp(locality, "i") };
   }
+  query.featured = true;
   const pgs = await PG.find(query);
   if (!pgs) {
     throw new ApiError(
@@ -92,4 +108,45 @@ export const getAllPG = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponse(200, pgs, "PGs found successfully"));
+});
+
+export const getAllPGsInfo = asyncHandler(async (req, res) => {
+  const pgs = await PG.find();
+  if (!pgs) {
+    throw new ApiError(
+      500,
+      "Something went wrong while fetching PG properties"
+    );
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, pgs, "PGs found successfully"));
+});
+
+export const countVerifiedPGs = asyncHandler(async (req, res) => {
+  const count = await PG.countDocuments({featured: true});
+  res
+    .status(200)
+    .json(new ApiResponse(200, count, "Counted Verified PGs successfully"));
+});
+
+export const countNotVerifiedPGs = asyncHandler(async (req, res) => {
+  const count = await PG.countDocuments({featured: false});
+  res
+    .status(200)
+    .json(new ApiResponse(200, count, "Counted Unverified PGs successfully"));
+});
+
+export const verifyPG = asyncHandler(async (req, res) => {
+  const { id, featured } = req.body;
+  await PG.findByIdAndUpdate(id, { $set: { featured } }, { new: true });
+  await axios.post("http://localhost:4000/backend-email-service/email", {
+    to: hostelData.email,
+    subject: "Property Listed Successfully!",
+    body: `   Thank you, your property has been listed!!`,
+    user: "Buddies.com",
+  });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "PG verified successfully"));
 });
