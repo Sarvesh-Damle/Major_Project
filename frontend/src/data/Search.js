db9cd7f5e8db79235aae13deae00a3a239ed2845
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 22:21:05 +0530

    Constant file for searching properties via Colleges or Companies

---
+++ frontend/src/data/Search.js
@@ -0,0 +1,30 @@
+export const searchData = [
+  {
+    name: "Vidyalankar Institute of Technology, Wadala",
+    description: "Engineering College in Wadala.",
+  },
+  {
+    name: "Institute of Chemical Technology, Matunga",
+    description: "Engineering College in Matunga.",
+  },
+  {
+    name: "Sardar Patel College of Engineering, Andheri",
+    description: "Engineering College in Andheri.",
+  },
+  {
+    name: "Veermata Jijabai Institute of Technology, Matunga",
+    description: "Engineering College in Matunga.",
+  },
+  {
+    name: "Deloitte, Airoli",
+    description: "Software Company in Airoli.",
+  },
+  {
+    name: "Godrej, Ghatkopar",
+    description: "Multi-Services Company in Ghatkopar.",
+  },
+  {
+    name: "Jio, Vashi",
+    description: "Software Company in Vashi.",
+  },
+];
