4eeab432e9751d7d6eb4e155d48e92635d1516ce
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 21:49:06 +0530

    Added routes for counting verified, unverified flat properties

--- backend/src/routes/flats.routes.js
+++ backend/src/routes/flats.routes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import Flat from "../models/flats.models.js";
 import {
+  countNotVerifiedFlats,
+  countVerifiedFlats,
   createFlat,
   deleteFlat,
   getAllFlat,
+  getAllFlatsInfo,
   getFlat,
   updateFlat,
+  verifyFlat,
 } from "../controllers/flats.controllers.js";
 // import { createError } from "../utils/error.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
@@ -16,7 +20,7 @@ const router = express.Router();
 
 // CREATE
 router.post(
-  "/add-property",
+  "/add-property-flat",
   verifyJWT,
   upload.fields([
     {
@@ -28,20 +32,25 @@ router.post(
 );
 
 // UPDATE
-router.put("/update-flat", upload.fields([
+router.put("/update-flat", verifyJWT, upload.fields([
   {
     name: "property_photos",
     maxCount: 5
   }
 ]), updateFlat);
 
+router.put("/verify-flat", verifyJWT, verifyAdmin, verifyFlat);
+
 // DELETE
-router.delete("/delete-flat", deleteFlat);
+router.delete("/delete-flat", verifyJWT, verifyAdmin, deleteFlat);
 
 // GET
 router.get("/find-flat", getFlat);
+router.get("/count-verified-flats", countVerifiedFlats);
+router.get("/count-unverified-flats", countNotVerifiedFlats);
 
 // GET ALL
 router.get("/find-all-flats", getAllFlat);
+router.get("/find-all-flats-info", verifyJWT, verifyAdmin, getAllFlatsInfo);
 
 export default router;
