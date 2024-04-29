0d1fa8de18361a3d334c0cb16ce6183843f8ec76
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 22:08:33 +0530

    UI changes with addition of react-typed package

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -1,7 +1,8 @@
 import SearchBar, { SearchBar2 } from "./SearchBar";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
+import {ReactTyped} from "react-typed";
 
 const Hero = () => {
     return (
@@ -11,14 +12,18 @@ const Hero = () => {
                 <div className="flex flex-col justify-center items-start gap-12 hero-left">
                     <div className="relative z-[1] hero-title">
                         <div className="h-16 w-16 bg-orange-gradient rounded-full absolute right-[28%] top-[-10%] z-[-1] orange-circle" />
-                        <motion.h1 initial={{y: "2rem", opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 2, type: "spring"}} className="font-semibold text-6xl max-md:text-[2.5rem] max-md:leading-10 max-lg:text-5xl max-lg:leading-[3rem] leading-[4rem]">Discover <br /> Most Suitable <br /> Property</motion.h1>
+                        <motion.h1 initial={{ y: "2rem", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 2, type: "spring" }} className="font-semibold text-6xl max-md:text-[2.5rem] max-md:leading-10 max-lg:text-5xl max-lg:leading-[3rem] leading-[4rem]">Discover <br /> Most Suitable <br /> Property</motion.h1>
                     </div>
                     <div className="flex flex-col justify-center gap-8 items-start text-xl text-gray-300 hero-des">
                         <span>
-                            Need a <span>pg/hostel/flat</span> ?
+                            <span className="text-orange-300 text-xl"><ReactTyped startWhenVisible strings={[
+                                "Need a PG?",
+                                "Need a Hostel?",
+                                "Need a Flat?",
+                            ]} typeSpeed={60} backSpeed={60} loop /></span>
                         </span>
                         <span>
-                            Search one near your college/company/business!
+                            Find one near your college/company/business!
                         </span>
                     </div>
                     <div className="flex flex-wrap gap-y-8 items-center justify-start gap-8 w-[100%] max-lg:justify-center max-lg:gap-6 stats">
@@ -31,10 +36,10 @@ const Hero = () => {
                             <span className="text-gray-300">Happy Customers</span>
                         </div>
                     </div>
-                </div> 
+                </div>
                 {/* right side */}
                 <div className="flex justify-center items-center flex-wrap gap-y-8 hero-right">
-                    <motion.div initial={{x: "7rem", opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 2, type: "spring"}} className="w-[30rem] h-[35rem] overflow-hidden rounded-t-full border-8 border-solid border-white border-opacity-10 max-lg:w-[95%] max-lg:h-[30rem] max-md:w-[95%] max-md:h-[25rem] image-container">
+                    <motion.div initial={{ x: "7rem", opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 2, type: "spring" }} className="w-[30rem] h-[35rem] overflow-hidden rounded-t-full border-8 border-solid border-white border-opacity-10 max-lg:w-[95%] max-lg:h-[30rem] max-md:w-[95%] max-md:h-[25rem] image-container">
                         <img src="https://res.cloudinary.com/sarvesh-damle/image/upload/v1710397575/Buddies_MajorProject/logos/hero-image_fs2tsc.png" alt="image" className="w-[100%] h-[100%]" />
                     </motion.div>
                 </div>
