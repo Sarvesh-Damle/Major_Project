1cd1be3254c9be8a2f70ee6c459304bf0486446a
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 22:13:44 +0530

    Added login check

--- frontend/src/components/ListProperties/PropertiesCard.jsx
+++ frontend/src/components/ListProperties/PropertiesCard.jsx
@@ -1,8 +1,12 @@
-import { useNavigate } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import {truncate} from "lodash";
+import { useContext } from "react";
+import { loginContext } from "../../provider/authContext";
+import { toast } from "react-toastify";
 
 const PropertiesCard = ({card}) => {
-    const navigate = useNavigate();
+    const navigate = useHistory();
+  const { isLoggedIn } = useContext(loginContext);
     let url = "hostels"
     if (card.pg_name) {
         url = "pgs"
@@ -10,10 +14,17 @@ const PropertiesCard = ({card}) => {
     if (card.flat_type) {
         url = "flats"
     }
+    const handleClick = () => {
+        if (isLoggedIn.login) {
+            navigate.push(`/${url}/${card._id}`)
+          } else {
+            toast.error("Please login!!", { position: "bottom-right" })
+          }
+    }
     return (
         <>
             <div className="flex flex-col w-72 h-96 gap-2 p-4 m-auto rounded-xl max-w-max max-h-max transition-all duration-300 ease-in hover:scale-105 hover:cursor-pointer hover:bg-gradient-to-b from-[#ffffff] to-[#eeeef7] shadow hover:shadow-lg r-card"
-            onClick={() => navigate(`/${url}/${card._id}`)}
+            onClick={handleClick}
             >
                 {card.property_photos.length > 0 && <img src={card.property_photos[0]} alt="property-image"
                     className="w-60 h-40 rounded-[10px]" />}
