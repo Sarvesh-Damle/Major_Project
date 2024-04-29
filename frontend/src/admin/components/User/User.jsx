ad6c73ca17d0723bd0ae4b34067dfae74b9ee977
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 22:04:16 +0530

    User Updation file in Admin Dashboard

---
+++ frontend/src/admin/components/User/User.jsx
@@ -0,0 +1,83 @@
+import axios from "axios";
+import Table from "../Table/Table.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import Loader from "../../../components/Pages/Loader.jsx"
+import ErrorComponent from "../../../components/Pages/ErrorComponent.jsx";
+import { UserDeleteButton, UserEditButton } from "../Buttons.jsx";
+import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min.js";
+
+const User = () => {
+  const navigate = useHistory();
+  const mutation = useMutation({
+    mutationKey: ['user-delete'],
+    mutationFn: (prop) => {
+      return axios.delete(`/api/v1/users/delete-user?id=${prop._id}`,
+        {withCredentials: true}
+      )
+    },
+    onSuccess (data) {
+      toast.success(data.data.message); 
+      refetch();
+    },
+    onError (error) {
+      let message = error.response?.data?.message;
+      toast.error(message);
+    }
+  })
+  const handleDelete = (prop) => {
+    mutation.mutate({_id: prop._id});
+  };
+  const handleEdit = (prop) => {
+    console.log(prop)
+    navigate.push(`/dashboard/users/edit-user/${prop._id}`);
+  };
+
+  let column = [
+    { field: "name", filter: true },
+    { field: "email", filter: true },
+    { field: "phoneNumber" },
+    {
+      field: "Edit",
+      cellRenderer: (prop) => (
+        // <EnrollButton func={handleEnroll} {...props} />
+        // <button><MdModeEdit size={20} className="text-gray-600"/></button>
+        <UserEditButton func={handleEdit} {...prop} />
+      ),
+    },
+    {
+      field: "Delete",
+      cellRenderer: (prop) => (
+        // <ViewAssigned func={view} {...props} />
+        // <button><MdDelete size={23} className="text-red-400"/></button>
+        <UserDeleteButton func={handleDelete} {...prop} />
+      ),
+    },
+  ];
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ["User"],
+    queryFn: async () => {
+      const response = await axios.get("/api/v1/users", { withCredentials: true });
+      return response.data;
+    }
+  })
+  
+  if (isLoading) return <div className="flex h-screen w-screen justify-center items-center"><Loader /></div>
+  if (isError) return <ErrorComponent />
+  return (
+    <div className="w-[100vw] h-[90vh] bg-slate-100">
+      <div className="mr-3">
+        <div className="flex justify-between items-center  h-[120px] ">
+          <div className="w-[250px] ml-5 ">
+            <h1 className="text-2xl">Users</h1>
+          </div>
+        </div>
+        <div className="mt-2">
+          {!isLoading && <Table rowData={data} colDefs={column} />}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default User
