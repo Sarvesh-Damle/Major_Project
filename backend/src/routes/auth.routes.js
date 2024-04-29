0e0724d3d79350fa2324f9829ce4ce13f2a8dd68
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 21:48:27 +0530

    Added route for login through google

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/auth.controllers.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, google } from "../controllers/auth.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
+
+router.post("/google", google);
 
 // secured routes
 router.post("/logout", verifyJWT, logoutUser);
