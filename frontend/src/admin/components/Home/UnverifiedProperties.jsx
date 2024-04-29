ceb153a8f38c371c38c51ce61453fc7f4df0718c
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 22:01:25 +0530

    Component for counting unverified properties

---
+++ frontend/src/admin/components/Home/UnverifiedProperties.jsx
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import ErrorComponent from "../../../components/Pages/ErrorComponent";
+import Loader from "../../../components/Pages/Loader";
+import axios from "axios";
+
+const UnverifiedProperties = () => {
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["Unverified-Hostels-Count"],
+        queryFn: async () => {
+            const response = await axios.get("/api/v1/hostels/count-unverified-hostels", { withCredentials: true });
+            return response.data;
+        }
+    })
+    const { data: data2, isLoading: isLoading2, isError: isError2 } = useQuery({
+        queryKey: ["Unverified-PGs-Count"],
+        queryFn: async () => {
+            const response = await axios.get("/api/v1/pgs/count-unverified-pgs", { withCredentials: true });
+            return response.data;
+        }
+    })
+    const { data: data3, isLoading: isLoading3, isError: isError3 } = useQuery({
+        queryKey: ["Unverified-Flats-Count"],
+        queryFn: async () => {
+            const response = await axios.get("/api/v1/flats/count-unverified-flats", { withCredentials: true });
+            return response.data;
+        }
+    })
+    if (isLoading || isLoading2 || isLoading3) return <Loader />
+    if (isError || isError2 || isError3) return <ErrorComponent />
+    return {
+        hostel: data.data || 0,
+        pg: data2.data || 0,
+        flat: data3.data || 0,
+    };
+};
+
+export default UnverifiedProperties
