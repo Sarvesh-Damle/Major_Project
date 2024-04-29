771d3eff81f744ebd49aeac5f0ff970b50369364
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 22:06:14 +0530

    Reset Password Page

--- frontend/src/components/Authentication/ResetPassword.jsx
+++ frontend/src/components/Authentication/ResetPassword.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 // import { toast } from "react-toastify";
 // import 'react-toastify/dist/ReactToastify.css';
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const ResetPassword = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const navigate = useNavigate();
+  const navigate = useHistory();
   const handleSubmit = async (e) => {
     e.preventDefault();
     navigate("/");
