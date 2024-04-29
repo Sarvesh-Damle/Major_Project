df8a5c9d769b495251e2df5072bcf64e180d15a9
Sarvesh <sarvesh.damle1@gmail.com>
2024-04-29 21:50:14 +0530

    Added routes for counting verified and unverified pg properties

--- backend/src/routes/pgs.routes.js
+++ backend/src/routes/pgs.routes.js
@@ -2,34 +2,57 @@ import express from "express";
 import PG from "../models/pgs.models.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { createPG, deletePG, getAllPG, getPG, updatePG } from "../controllers/pgs.controllers.js";
+import {
+  countNotVerifiedPGs,
+  countVerifiedPGs,
+  createPG,
+  deletePG,
+  getAllPG,
+  getAllPGsInfo,
+  getPG,
+  updatePG,
+  verifyPG,
+} from "../controllers/pgs.controllers.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // CREATE
-router.post("/add-property", verifyJWT, upload.fields([
+router.post(
+  "/add-property-pg",
+  verifyJWT,
+  upload.fields([
     {
       name: "property_photos",
-      maxCount: 5
-    }
-  ]), createPG);
+      maxCount: 5,
+    },
+  ]),
+  createPG
+);
 
 // UPDATE
-router.put("/update-pg", upload.fields([
+router.put(
+  "/update-pg",
+  upload.fields([
     {
       name: "property_photos",
-      maxCount: 5
-    }
-  ]), updatePG);
-
+      maxCount: 5,
+    },
+  ]),
+  updatePG
+);
+router.put("/verify-pg", verifyJWT, verifyAdmin, verifyPG);
 // DELETE
-router.delete("/delete-pg", deletePG);
+router.delete("/delete-pg", verifyJWT, verifyAdmin,deletePG);
 
 // GET
 router.get("/find-pg", getPG);
+router.get("/count-verified-pgs", countVerifiedPGs);
+router.get("/count-unverified-pgs", countNotVerifiedPGs);
 
 // GET ALL
 router.get("/find-all-pgs", getAllPG);
+router.get("/find-all-pgs-info", verifyJWT, verifyAdmin, getAllPGsInfo);
 
 // pending...
 // router.get("/countByAddress", countByAddress);
