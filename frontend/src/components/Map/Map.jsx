cdfca3224093e4b6fa8b4d68c757a4f3f5349269
Sarvesh-Damle01 <sarvesh.damle1@gmail.com>
2026-01-27 14:45:29 +0530

    feat: enable home page sections (C1, C2, C3)
    - C1: Enable Featured Properties section with city cards
    - C2: Enable Flatmates section with styled coming-soon placeholder
    - C3: Enable Map section centered on Mumbai with proper container
    - Clean up commented-out code from Home.jsx

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -2,11 +2,11 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 // import "leaflet/dist/leaflet.css";
 
 const Map = () => {
-  const position = [51.505, -0.09];
+  const position = [19.076, 72.8777]; // Mumbai
   return (
     <MapContainer
       center={position}
-      zoom={13}
+      zoom={11}
       scrollWheelZoom={false}
       className='w-[100%] h-[100%] rounded-[20px] map'
     >
@@ -14,11 +14,6 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      {/* <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker> */}
     </MapContainer>
   );
 };
